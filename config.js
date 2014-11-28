a3872fbe3fbb225fdffa8503b47a9f9dc6d43a39
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-28 10:34:28 -0800

    Built for production.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://yourawesomewebsite.azurewebsites.net/',
+        url: 'http://ghostest.azurewebsites.net/',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -32,7 +32,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blogs published URL.
-        url: 'http://yourawesomewebsite.azurewebsites.net/',
+        url: 'http://ghostest.azurewebsites.net/',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
